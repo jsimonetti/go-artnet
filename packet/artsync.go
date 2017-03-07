f51252d2bc0bbe8ced468cf886698b5efea6d48d
Jeroen Simonetti <jeroen.simonetti@securelink.nl>
2017-03-07 22:28:12 +0100

    Add ArtSync packet

--- packet/artsync.go
+++ packet/artsync.go
@@ -1,6 +1,84 @@
 package packet
 
+import (
+	"bytes"
+	"encoding/binary"
+	"fmt"
+
+	"github.com/jsimonetti/go-artnet/packet/code"
+	"github.com/jsimonetti/go-artnet/version"
+)
+
+var _ ArtNetPacket = &ArtSyncPacket{}
+
 // ArtSyncPacket contains an ArtSync Packet.
+//
+// The ArtSync packet can be used to force nodes to synchronously output ArtDmx packets
+// to their outputs. This is useful in video and media-wall applications.
+// A controller that wishes to implement synchronous transmission will unicast multiple
+// universes of ArtDmx and then broadcast an ArtSync to synchronously transfer all the
+// ArtDmx packets to the nodes’ outputs at the same time.
+//
+// Packet Strategy:
+//  Controller -  Receive:            No Action
+//                Unicast Transmit:   Not Allowed
+//                Broadcast Transmit: Controller broadcasts this packet to synchronously
+//                                    transfer previous ArtDmx packets to Node’s output
+//  Node -        Receive:            Transfer previous ArtDmx packets to output
+//                Unicast Transmit:   Not Allowed
+//                Broadcast Transmit: Not Allowed
+//  MediaServer - Receive:            Transfer previous ArtDmx packets to output
+//                Unicast Transmit:   Not Allowed
+//                Broadcast Transmit: Not Allowed
 type ArtSyncPacket struct {
+	// Inherit the Header header
 	Header
+
+	// this packet type contains a version
+	version [2]byte
+
+	// aux are auxiliary bytes transmitted as zero
+	aux [2]byte
+}
+
+// NewArtSyncPacket returns an ArtNetPacket with the correct OpCode
+func NewArtSyncPacket() *ArtSyncPacket {
+	return &ArtSyncPacket{
+		Header: Header{
+			OpCode: code.OpSync,
+			id:     ArtNet,
+		},
+		version: version.Bytes(),
+	}
+}
+
+// MarshalBinary marshals an ArtSyncPacket into a byte slice.
+func (p *ArtSyncPacket) MarshalBinary() ([]byte, error) {
+	var buf bytes.Buffer
+	if err := binary.Write(&buf, binary.BigEndian, p); err != nil {
+		return nil, err
+	}
+	return buf.Bytes(), p.validate()
+}
+
+// UnmarshalBinary unmarshals the contents of a byte slice into an ArtSyncPacket.
+func (p *ArtSyncPacket) UnmarshalBinary(b []byte) error {
+	if err := p.Header.unmarshal(b[:10]); err != nil {
+		return err
+	}
+	if len(b) != 14 {
+		return fmt.Errorf("invalid packet length received. want: 14, got: %d", len(b))
+	}
+	p.version = [2]byte{b[11], b[10]}
+	p.aux = [2]byte{b[12], b[13]}
+
+	return p.validate()
+}
+
+// artPacket is an empty method to sattisfy the ArtNetPacket interface.
+func (p *ArtSyncPacket) validate() error {
+	if p.OpCode != code.OpSync {
+		return errInvalidOpCode
+	}
+	return nil
 }
