32f51f4f66e44b10803c39fbc3d7e3b484ea802f
Jeroen Simonetti <jeroen.simonetti@securelink.nl>
2017-03-18 15:26:11 +0100

    Add functions to artnet package

--- artnet.go
+++ artnet.go
@@ -1,1 +1,18 @@
 package artnet
+
+import (
+	"net"
+
+	"github.com/jsimonetti/go-artnet/node"
+	"github.com/jsimonetti/go-artnet/packet/code"
+)
+
+// NewController returns an instance of an ArtNet Controller
+func NewController(name string, ip net.IP) *node.Controller {
+	return node.NewController(name, ip)
+}
+
+// NewNode returns an instance of an ArtNet Node
+func NewNode(name string, style code.StyleCode, ip net.IP) *node.Node {
+	return node.NewNode(name, style, ip)
+}
--- example/controller/main.go
+++ example/controller/main.go
@@ -3,11 +3,11 @@ package main
 import (
 	"net"
 
-	"github.com/jsimonetti/go-artnet/node"
+	"github.com/jsimonetti/go-artnet"
 )
 
 func main() {
-	c := node.NewController("controller-1", net.ParseIP("2.12.12.12"))
+	c := artnet.NewController("controller-1", net.ParseIP("2.12.12.12"))
 	if err := c.Start(); err != nil {
 		panic(err)
 	}
