3cffd564359dac8c6b9d88c0a830b87e3d299093
72nd <msg@frg72.com>
2024-01-20 16:35:49 +0100

    packet unmarshal now returns error on all packet types which are currently not implemented
    Signed-off-by: 72nd <msg@frg72.com>

--- packet/packet.go
+++ packet/packet.go
@@ -14,6 +14,8 @@ func Unmarshal(b []byte) (p ArtNetPacket, err error) {
 		return
 	}
 
+	notImplErr := fmt.Errorf("unimplemented opcode %#v found", h.OpCode)
+
 	switch h.OpCode {
 	case code.OpPoll:
 		p = &ArtPollPacket{}
@@ -33,38 +35,61 @@ func Unmarshal(b []byte) (p ArtNetPacket, err error) {
 	case code.OpAddress:
 		p = &ArtAddressPacket{}
 	case code.OpInput:
+		return nil, notImplErr
 	case code.OpTodRequest:
+		return nil, notImplErr
 	case code.OpTodData:
+		return nil, notImplErr
 	case code.OpTodControl:
+		return nil, notImplErr
 	case code.OpRdm:
+		return nil, notImplErr
 	case code.OpRdmSub:
+		return nil, notImplErr
 	case code.OpMedia:
+		return nil, notImplErr
 	case code.OpMediaPatch:
+		return nil, notImplErr
 	case code.OpMediaControl:
+		return nil, notImplErr
 	case code.OpMediaContrlReply:
+		return nil, notImplErr
 	case code.OpTimeCode:
 		p = &ArtTimeCodePacket{}
 	case code.OpTimeSync:
+		return nil, notImplErr
 	case code.OpTrigger:
 		p = &ArtTriggerPacket{}
 	case code.OpDirectory:
+		return nil, notImplErr
 	case code.OpDirectoryReply:
+		return nil, notImplErr
 	case code.OpVideoSetup:
+		return nil, notImplErr
 	case code.OpVideoPalette:
+		return nil, notImplErr
 	case code.OpVideoData:
+		return nil, notImplErr
 	case code.OpMacMaster:
+		return nil, notImplErr
 	case code.OpMacSlave:
+		return nil, notImplErr
 	case code.OpFirmwareMaster:
+		return nil, notImplErr
 	case code.OpFirmwareReply:
+		return nil, notImplErr
 	case code.OpFileTnMaster:
+		return nil, notImplErr
 	case code.OpFileFnMaster:
+		return nil, notImplErr
 	case code.OpFileFnReply:
+		return nil, notImplErr
 	case code.OpIPProg:
 		p = &ArtIPProgPacket{}
 	case code.OpIPProgReply:
 		p = &ArtIPProgReplyPacket{}
 	default:
-		return nil, fmt.Errorf("unimplemented opcode %#v found", h.OpCode)
+		return nil, notImplErr
 	}
 
 	err = p.UnmarshalBinary(b)
